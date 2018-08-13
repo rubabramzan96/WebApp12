849729e07f25b59abe1364cbc773477bd0c99846
rubabramzan96 <rubabramzan96@gmail.com>
2018-08-13 19:53:00 +0100

    1 e added books and member

--- WebApplication1/WebApplication1/Migrations/LibraryMigrations/Configuration.cs
+++ WebApplication1/WebApplication1/Migrations/LibraryMigrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace WebApplication1.Migrations.LibraryMigrations
 {
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,18 +16,89 @@ namespace WebApplication1.Migrations.LibraryMigrations
 
         protected override void Seed(WebApplication1.Models.LibraryContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            seedMember(context);
+            seedBooks(context);
+        }
+        private void seedMember(LibraryContext context)
+        {
+            #region Member1
+            context.Members.AddOrUpdate(c => c.MemberId,
+                        new Member
+                        {
+                            FirstName = "sararh",
+                            SecondName = "aaaa",
+                            DateJoined = DateTime.Now,
+                            MemberId = 1,
+                            //Members = SeedMembers(context),
+                            //      memberLoan = new List<Loan>()
+                            /*
+                                                       memberLoan = new List<Loan>()
+                                       {	// Create a Loan out on book to one memnber
+                                                   new Loan { LoanId = 1,
+                                                      MemberId = 1,
+                                                      BookId=1,
+                                                      // Update attendees with a method similar to the SeedClubMembers 
+                                                      // See below
+                                                   }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+                                       } */
+                        });
+            #endregion
+            #region Member2
+
+            context.Members.AddOrUpdate(c => c.MemberId,
+                        new Member
+                        {
+                            FirstName = "Tina",
+                            SecondName = "bbbbb",
+                            DateJoined = DateTime.Now,
+                            MemberId = 2,
+                            //Member = SeedMembers(context),
+                            //      memberLoan = new List<Loan>()
+                        });
+            #endregion
+            context.SaveChanges(); // NOTE EF will update the relevant foreign key fields in the clubs, club events and member tables based on the attributes
+        }
+        private void seedBooks(LibraryContext context)
+        {
+            #region Book1
+            context.books.AddOrUpdate(c => c.BookId,
+                        new Book
+                        {
+                            BookId = 1,
+                            Title = "circle of friends ",
+                            ISBN = "12345678",
+                            Author = "Meave Binchy",
+
+                            //Members = SeedMembers(context),
+                            //      memberLoan = new List<Loan>()
+                        });
+            #endregion
+            #region Book2
+            context.books.AddOrUpdate(c => c.BookId,
+                        new Book
+                        {
+                            BookId = 2,
+                            Title = " friends ",
+                            ISBN = "23456789",
+                            Author = "aaaaaaaaa",
+                            //Members = SeedMembers(context),
+                            //      memberLoan = new List<Loan>()
+                        });
+            #endregion
+            #region Book3
+            context.books.AddOrUpdate(c => c.BookId,
+                        new Book
+                        {
+                            BookId = 3,
+                            Title = " Circle ",
+                            ISBN = "34567890",
+                            Author = "bbbbbb",
+                            //Members = SeedMembers(context),
+                            //      memberLoan = new List<Loan>()
+                        });
+            #endregion
+            context.SaveChanges(); // NOTE EF will update the relevant foreign key fields in the clubs, club events and member tables based on the attributes
         }
     }
 }
