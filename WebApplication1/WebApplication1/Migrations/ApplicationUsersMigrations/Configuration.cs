4b276b719bfe8e10359ae370831766f1e8613039
rubabramzan96 <rubabramzan96@gmail.com>
2018-08-13 19:48:59 +0100

    1 d

--- WebApplication1/WebApplication1/Migrations/ApplicationUsersMigrations/Configuration.cs
+++ WebApplication1/WebApplication1/Migrations/ApplicationUsersMigrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace WebApplication1.Migrations.ApplicationUsersMigrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,18 +18,54 @@ namespace WebApplication1.Migrations.ApplicationUsersMigrations
 
         protected override void Seed(WebApplication1.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var manager =
+               new UserManager<ApplicationUser>(
+                   new UserStore<ApplicationUser>(context));
+            var roleManager =
+               new RoleManager<IdentityRole>(
+                   new RoleStore<IdentityRole>(context));
+            context.Roles.AddOrUpdate(r => r.Name,
+               new IdentityRole { Name = "Librarian" }
+               );
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "Member" }
+                );
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            PasswordHasher ps = new PasswordHasher();
+            context.Users.AddOrUpdate(u => u.UserName,
+               new ApplicationUser
+               {
+                   UserName = "einstein.albert@itsligo.ie",
+                   Email = "einstein.albert@itsligo.ie",
+                   EmailConfirmed = true,
+                   DateJoined = DateTime.Now,
+                   SecurityStamp = Guid.NewGuid().ToString(),
+                   PasswordHash = ps.HashPassword("ITSligo$1")
+               });
+            context.Users.AddOrUpdate(u => u.UserName,
+               new ApplicationUser
+               {
+                   UserName = "blogs.joe@itsligo.ie",
+                   Email = "blogs.joe@itsligo.ie",
+                   EmailConfirmed = true,
+                   DateJoined = DateTime.Now,
+                   SecurityStamp = Guid.NewGuid().ToString(),
+                   PasswordHash = ps.HashPassword("ITSligo$2")
+               });
+            context.SaveChanges();
+            //librarian role
+            ApplicationUser Librarian = manager.FindByEmail("einstein.albert@itsligo.ie");
+            if (Librarian != null)
+            {
+                manager.AddToRoles(Librarian.Id, new string[] { "Librarian", "Member" });
+            }
+            //member role
+            ApplicationUser Member = manager.FindByEmail("blogs.joe@itsligo.ie");
+            if (Member != null)
+            {
+                manager.AddToRoles(Member.Id, new string[] { "Librarian", "Member" });
+            }
+
         }
     }
 }
